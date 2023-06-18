d1828e7686c958bd6f246925ec0a2bcb7ee67fea
Chris Kanich <kaytwo@gmail.com>
2023-06-17 20:55:08 -0500

    debugged

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ apt-get -y install bash
 
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash - 
 
-apt install -y nodejs
+# node, jq, playwright deps
+apt-get install -y nodejs jq libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 \
+ libcups2 libdrm2 libxkbcommon0 libatspi2.0-0 libxcomposite1 libxdamage1 \
+ libxfixes3 libxrandr2 libgbm1 libpango-1.0-0 libcairo2
 # todo: might need build-essential etc if node needs native binaries built
